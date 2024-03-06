1a009038a883c5fb9016e0c4b7a59b6512a8ff33
Ritis Barauskas <perm300ru@yandex.ru>
2024-03-06 22:56:35 +0300

    Создана модель пользователей и настроена админка

--- doit_news/users/admin.py
+++ doit_news/users/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from .models import DoitUser
+
+
+@admin.register(DoitUser)
+class DoitUserAdmin(UserAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'is_staff', 'is_active', 'date_joined')
+    list_filter = ('is_staff', 'is_active')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('date_joined',)
+
+
+admin.site.unregister(Group)
--- doit_news/users/models.py
+++ doit_news/users/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from .constants import DEFAULT_LENGTH_CHAR_FIELD
+
+
+class DoitUser(AbstractUser):
+    email = models.EmailField(unique=True, verbose_name='Email')
+    first_name = models.CharField(max_length=DEFAULT_LENGTH_CHAR_FIELD, verbose_name='Имя')
+    last_name = models.CharField(max_length=DEFAULT_LENGTH_CHAR_FIELD, verbose_name='Фамилия')
+    username = models.CharField(max_length=DEFAULT_LENGTH_CHAR_FIELD, unique=True, verbose_name='Имя пользователя')
+    password = models.CharField(max_length=DEFAULT_LENGTH_CHAR_FIELD, verbose_name='Пароль')
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        ordering = ['date_joined']
+
+    def __str__(self):
+        return self.email
